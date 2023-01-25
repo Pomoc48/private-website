a12beeeefed94a801d63949dc6f15d9632a97b36
Pomoc48 <lukawski.mikolaj@gmail.com>
2023-01-25 15:53:00 +0100

    Add basic top navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography, Container, Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <TopBar text='Mikołaj Łukawski' />
+      <Container>
+        <PageTitle text='Hello world!' />
+      </Container>
+    </Box>
   );
 }
 
 export default App;
+
+function PageTitle(props: { text: string }) {
+  return (
+    <Typography variant="h2" sx={{ py: 8 }}>
+      {props.text}
+    </Typography>
+  );
+}
+
+function TopBar(props: { text: string }) {
+  return (
+    <Box className="TopBar">
+      <Container>
+        <Typography variant="h5" sx={{ py: 3 }} >
+          {props.text}
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
