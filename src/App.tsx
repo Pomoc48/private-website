5a8e71fdcca3ca6d2a96aa241ad133440bfac621
Pomoc48 <lukawski.mikolaj@gmail.com>
2023-01-26 15:28:57 +0100

    Update buttons

--- src/App.tsx
+++ src/App.tsx
@@ -44,15 +44,11 @@ function Description() {
   return (
     <Box sx={{ maxWidth: 600 }}>
       <Typography sx={{ pt: 5 }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Varius quam quisque id diam vel quam elementum pulvinar.
-        Quam vulputate dignissim suspendisse in est ante in nibh mauris.
-        Porttitor eget dolor morbi non arcu risus quis varius quam.
+        I'm a junior frontend developer, that mainly specializes in Flutter and has a good understanding of creating responsive, consistent and intuitive UI.
       </Typography>
       <Typography sx={{ pt: 2 }}>
-        Eu facilisis sed odio morbi quis commodo.
-        Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit.
-        Tellus cras adipiscing enim eu.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </Typography>
     </Box>
   );
@@ -61,8 +57,8 @@ function Description() {
 function LinkButtons() {
   return (
     <Stack direction="row" spacing={2} sx={{ py: 5 }}>
-      <Button variant="outlined" href="https://github.com/Pomoc48" target="_blank" >Github</Button>
-      <Button variant="outlined" href="#" disabled>Linkedin</Button>
+      <Button variant="outlined" href="https://github.com/Pomoc48" target="_blank">Portfolio</Button>
+      <Button variant="outlined" href="mailto:lukawski.mikolaj@gmail.com">Contact me</Button>
     </Stack>
   )
 }
