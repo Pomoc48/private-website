f2d56277337ec8e89ba6667c58c473fe63a8b397
Pomoc48 <lukawski.mikolaj@gmail.com>
2023-01-31 16:02:52 +0100

    Replace svg with tsx

---
+++ src/circles/C3.tsx
@@ -0,0 +1,61 @@
+function C3() {
+  return (
+    <svg
+      className="C3"
+      xmlns="http://www.w3.org/2000/svg"
+      width="640"
+      height="640"
+      version="1.1"
+      viewBox="0 0 169.333 169.333"
+    >
+      <g fill="none" strokeDasharray="none" strokeLinejoin="round">
+        <circle
+          cx="92.849"
+          cy="81.316"
+          r="70"
+          stroke="#00695c"
+          strokeOpacity="0.847"
+          strokeWidth="8"
+          display="none"
+          opacity="0.5"
+          stopColor="#000"
+        ></circle>
+        <circle
+          cx="80.563"
+          cy="76.438"
+          r="71"
+          stroke="#00897a"
+          strokeOpacity="0.851"
+          strokeWidth="6"
+          display="inline"
+          opacity="0.5"
+          stopColor="#000"
+        ></circle>
+        <circle
+          cx="86.437"
+          cy="90.188"
+          r="71.5"
+          stroke="#26a699"
+          strokeOpacity="0.847"
+          strokeWidth="5"
+          display="none"
+          opacity="0.5"
+          stopColor="#000"
+        ></circle>
+        <circle
+          cx="79.645"
+          cy="87.042"
+          r="72.5"
+          stroke="#80cbc4"
+          strokeOpacity="0.846"
+          strokeWidth="3"
+          display="none"
+          opacity="0.5"
+          stopColor="#000"
+        ></circle>
+      </g>
+    </svg>
+  );
+}
+
+export default C3;
